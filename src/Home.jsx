2510182316d63fffc0117861ae67b877d22154ae
Hannah Manes <baixinhamanes@hotmail.com>
2026-03-18 21:12:35 -0300

    useEffect error handling, exhibition limits for characters

--- src/Home.jsx
+++ src/Home.jsx
@@ -5,15 +5,27 @@ import EpisodesList from "./components/EpisodesList";
 function Home() {
   const [characters, setCharacters] = useState(null);
   const [episodes, setEpisodes] = useState(null);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     fetch("https://thesimpsonsapi.com/api/characters")
-      .then((res) => res.json())
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error(
+            "Resources not found. Check the address and try again.",
+          );
+        }
+        return res.json();
+      })
       .then((data) => {
         const charactersList = data.results;
         const episodesList = data.results;
         setEpisodes(episodesList);
         setCharacters(charactersList);
+        setError(null);
+      })
+      .catch((err) => {
+        setError(err.message);
       });
   }, []);
 
@@ -26,12 +38,17 @@ function Home() {
         Here you can find everything about the characters and your favorite
         episodes
       </p>
+      <div className="error-message">
+        <p>{error}</p>
+      </div>
       <section className="characters">
         {characters && (
           <CharacterList
             title={"Characters"}
             characters={characters}
             url={"https://cdn.thesimpsonsapi.com/500"}
+            num={6}
+            phraseNum={3}
           />
         )}
         {/* <div>
--- src/components/CharacterList.jsx
+++ src/components/CharacterList.jsx
@@ -1,17 +1,22 @@
-function CharacterList({ title, characters, url }) {
+function CharacterList({ title, characters, url, num, phraseNum }) {
   return (
-    <div className="characters-list">
+    <div className="char-container">
       <h2>{title}</h2>
       {characters.map((char) => (
-        <ul>
-          <li key={char.id}>
-            <img
-              src={url + char.portrait_path}
-              alt={`Desenho do personagem ${char.name}`}
-              width={100}
-            />
-            <p>{char.name}</p>
-          </li>
+        <ul className="char-list">
+          {char.id < num && (
+            <li key={char.id}>
+              <p>{char.name}</p>
+              <img
+                src={url + char.portrait_path}
+                alt={`Desenho do personagem ${char.name}`}
+                width={100}
+              />
+              <p>{char.age}</p>
+              <p>{char.occupation}</p>
+              <p>{char.phrases[phraseNum]}</p>
+            </li>
+          )}
         </ul>
       ))}
     </div>
